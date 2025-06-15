91940ae8e59436f11436c2b6833150eb3110e212
wajiha-kulsum <wajihakulsum786@gmail.com>
2025-06-16 00:13:43 +0530

    my second program

---
+++ age.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cin >> age;
+    if (age >= 18){
+        cout << "you are an adult";
+    }
+    else{
+        cout << "youare not an adult";
+    }
+    return 0;
+}
