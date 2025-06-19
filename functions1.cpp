d1d447cf44bac7e2ea9da284945682a01f4628c9
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-06-19 15:10:54 +0530

    Update functions1.cpp

--- functions1.cpp
+++ functions1.cpp
@@ -16,3 +16,24 @@ int main(){
     cout << w << endl;
     return 0;
 }
+
+
+
+//pass by reference
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void doWork(string &w){  // & is addede to pass the original value
+    w[0] = 'v';
+    cout << w << endl;
+    
+}
+
+int main(){
+    string w = "wajiha";
+    doWork(w);
+    cout << w << endl;
+    return 0;
+}
