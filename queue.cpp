803472d4f846de61a51632ca09f9047575780746
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-06-24 20:41:59 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void explainQueue() {
+    queue<int> q;
+    q.push(1);     // {1}
+    q.push(2);     // {1, 2}
+    q.emplace(4);  // {1, 2, 4}
+
+    q.back() += 5;  // q.back() is 4 → becomes 9
+
+    cout << q.back() << endl;   // prints 9
+
+    // Queue is now: {1, 2, 9}
+    cout << q.front() << endl;  // prints 1
+
+    q.pop();                    // removes 1 → {2, 9}
+    cout << q.front() << endl;  // prints 2
+}
+
+int main() {
+    explainQueue();  // call the function
+    return 0;
+}
